68ff67afef16eb967d473d99dfd06666c5649bd3
David Grammatico <david.lugdunum@gmail.com>
2024-01-08 07:53:54 +0100

    fix : add bundler metro for web

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,11 +1,32 @@
-import { Link } from 'expo-router';
-import { View, Text } from 'react-native';
+import {
+	Link,
+	router,
+	useGlobalSearchParams,
+	useSearchParams,
+} from 'expo-router';
+import { View, Text, Pressable } from 'react-native';
 
 export default function SettingsScreen() {
+	const params = useGlobalSearchParams();
+	const handlePressed = () => {
+		router.push('/');
+	};
+
+	const handleSetParams = () => {
+		router.setParams({ test: 'test' });
+	};
+
+	console.log(params);
 	return (
 		<View>
 			<Text>Ceci est une page de settings ...</Text>
 			<Link href={'/'}>Home</Link>
+			<Pressable onPress={handlePressed}>
+				<Text>Valider les modifications</Text>
+			</Pressable>
+			<Pressable onPress={handleSetParams}>
+				<Text>Changer les paramètres</Text>
+			</Pressable>
 		</View>
 	);
 }
